b5007bad94df013a68a886a153f919835862b890
Mert Yesiloglu <mertyesiloglu2@gmail.com>
2026-02-11 15:44:25 +0300

    Add my first C script with comments.
    This file contains basic C code and English comments explaining the logic.

---
+++ hello.c
@@ -0,0 +1,10 @@
+// This is Mert's first C project.
+// Learning C step by step.
+
+#include <stdio.h> // include the standard input output library
+int main() { // main function is the entry point of the program
+    printf("Learning C step by step\n"); // print the string to the console
+    return 0; // return 0 to the operating system means the program is successful
+} // end of the main function
+
+// This is comment in C.
